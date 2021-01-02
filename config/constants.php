aeb5e6092e967eaf2d2dfed56e1155edae461f4e
andifajarlah@gmail.com <andifajarlah@gmail.com>
2021-01-03 06:17:53 +0800

    kontak

--- config/constants.php
+++ config/constants.php
@@ -4,6 +4,6 @@ return [
     'gambar' => '/uploads/kontak/kontak.png',
     'alamat' => 'Jl. Wage Walker No. 17 Bunklotok Batujai, Praya Barat, Lombok Tengah',
     'telpon' => '0819-1728-7555 / 0823-4143-9988',
-    'mail' => 'admin@mahesaindopersada.com',
+    'mail' => 'admin@miproperti.com',
     'tentang' => 'Telah lama berkarya dan berperan aktif dalam pembangunan di tanah air. Kepercayaan yang diberikan dari berbagai pihak telah menciptakan berbagai karya konstruksi besar yang tesebar di berbagai sektor konstruksi bangunan gedung dan sipil'
 ];
