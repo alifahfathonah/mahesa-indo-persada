de7af951289ac865bab2ba0bb272475313950056
Andi Fajar Nugraha <andifajarlah@gmail.com>
2021-01-08 15:38:28 +0800

    logotu

--- resources/views/backend/includes/header.blade.php
+++ resources/views/backend/includes/header.blade.php
@@ -9,7 +9,7 @@
     <ul class="navbar-nav ml-auto">
         <li class="nav-item">
             <a class="nav-link" href="/logout">
-            <i class="fas fa-sign-out-alt"></i>
+            Logout <i class="fas fa-sign-out-alt"></i>
             </a>
         </li>
     </ul>
